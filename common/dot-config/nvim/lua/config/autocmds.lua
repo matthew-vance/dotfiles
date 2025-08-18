bd2bf730bf9681ed945d0f2c8681d20498c053dd
Matthew Vance <mavance44@gmail.com>
2025-08-18 17:33:56 -0400

    add autocmd for lspattach

--- common/dot-config/nvim/lua/config/autocmds.lua
+++ common/dot-config/nvim/lua/config/autocmds.lua
@@ -5,3 +5,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- LSP attach optimizations
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP buffer mappings and settings",
+	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+	callback = function(event)
+		local bufnr = event.buf
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+		-- Enable inlay hints if supported
+		if client and client.server_capabilities.inlayHintProvider then
+			vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+		end
+	end,
+})
