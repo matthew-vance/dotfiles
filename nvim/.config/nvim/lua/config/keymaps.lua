45ef63b4cb0618f176d343328d82a3fd9d3cf3f2
Matthew Vance <mavance44@gmail.com>
2026-04-19 11:04:45 -0400

    add some lsp keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,13 @@ map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlights" })
 
 map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 map("n", "<leader>D", function() Snacks.picker.diagnostics() end, { desc = "Search diagnostics" })
+map("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, { desc = "Next diagnostic" })
+map("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, { desc = "Prev diagnostic" })
+
+map("n", "grn", vim.lsp.buf.rename, { desc = "LSP rename" })
+map("n", "gra", vim.lsp.buf.code_action, { desc = "LSP code action" })
+map("n", "grr", function() Snacks.picker.lsp_references() end, { desc = "LSP references" })
+map("n", "gri", function() Snacks.picker.lsp_implementations() end, { desc = "LSP implementations" })
 
 map("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
 map("n", "<C-j>", "<C-w>j", { desc = "Move to down split" })
